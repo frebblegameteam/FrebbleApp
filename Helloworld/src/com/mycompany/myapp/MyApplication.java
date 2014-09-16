2f50ce513f32964781c1ab4121c50e6f25143144
Xander <xander_loomans@hotmail.com>
2014-09-16 16:13:58 +0200

    Frebble Project

---
+++ Helloworld/src/com/mycompany/myapp/MyApplication.java
@@ -0,0 +1,93 @@
+<<<<<<< HEAD
+package com.mycompany.myapp;
+
+
+import com.codename1.io.Log;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import userclasses.StateMachine;
+
+public class MyApplication {
+   
+    private Form current;
+
+    public void init(Object context) {
+        // Pro users - uncomment this code to get crash reports sent to you automatically
+        /*Display.getInstance().addEdtErrorHandler(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                evt.consume();
+                Log.p("Exception in AppName version " + Display.getInstance().getProperty("AppVersion", "Unknown"));
+                Log.p("OS " + Display.getInstance().getPlatformName());
+                Log.p("Error " + evt.getSource());
+                Log.p("Current Form " + Display.getInstance().getCurrent().getName());
+                Log.e((Throwable)evt.getSource());
+                Log.sendLog();
+            }
+        });*/
+    
+    }
+
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+        new StateMachine("/theme");        
+    }
+
+    public void stop() {
+        current = Display.getInstance().getCurrent();
+    }
+    
+    public void destroy() {
+    }
+}
+=======
+package com.mycompany.myapp;
+
+
+import com.codename1.io.Log;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import userclasses.StateMachine;
+
+public class MyApplication {
+   
+    private Form current;
+
+    public void init(Object context) {
+        // Pro users - uncomment this code to get crash reports sent to you automatically
+        /*Display.getInstance().addEdtErrorHandler(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                evt.consume();
+                Log.p("Exception in AppName version " + Display.getInstance().getProperty("AppVersion", "Unknown"));
+                Log.p("OS " + Display.getInstance().getPlatformName());
+                Log.p("Error " + evt.getSource());
+                Log.p("Current Form " + Display.getInstance().getCurrent().getName());
+                Log.e((Throwable)evt.getSource());
+                Log.sendLog();
+            }
+        });*/
+    
+    }
+
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+        new StateMachine("/theme");        
+    }
+
+    public void stop() {
+        current = Display.getInstance().getCurrent();
+    }
+    
+    public void destroy() {
+    }
+}
+>>>>>>> c930f2b116863ca9bf765358b6fa910befef2f85
